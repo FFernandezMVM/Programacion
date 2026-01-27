1794544f699fac6f82e3740bcd8c2a06112d6c18
Fran <ffernandez@institutmvm.cat>
2026-01-27 12:56:08 +0100

    Prueba de fogo

---
+++ future_motors.py
@@ -0,0 +1,23 @@
+class Vehicle:
+    def __init__(self, matricula, model, kms_inicials):
+        self.matricula = matricula
+        self._model = model       # Protegit (un guió)
+        self.__kms = kms_inicials # Privat (dos guions)
+
+    # TASCA: Fes el Getter per llegir els kms (ja que és privat)
+    def llegir_kms(self):
+        return self.__kms
+        pass
+
+    # TASCA: Fes el Setter amb seguretat anti-frau
+    def actualitzar_kms(self, nous_kms): 
+        if nous_kms <= self.__kms:
+            print("No se pueden bajar los kilometros. ")
+            return False
+        else:
+            self._kms = nous_kms
+            print("Kilometros actualizados.")
+            return True
+        # Si nous_kms és més petit que self.__kms, retorna False.
+        # Si és correcte, actualitza i retorna True.
+        pass
